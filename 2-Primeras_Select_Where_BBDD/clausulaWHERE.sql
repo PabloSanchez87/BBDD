d32592afc7cf3ac0ad80d99de9679eac1be8e88b
PabloSanchez <sancheztorrespablo@gmail.com>
2024-07-14 18:59:32 +0200

    Ejercicios WHERE Resueltos.

--- 2-Primeras_Select_Where_BBDD/clausulaWHERE.sql
+++ 2-Primeras_Select_Where_BBDD/clausulaWHERE.sql
@@ -45,7 +45,61 @@ Tipos de datos en MySQL
 5. JSON
 */
 
+/* Ejericicios
+1. Descargar la base de datos ‘sakila’ e instalarla en MySQL Workbench.
+2. Seleccionar los campos ‘address’ y ‘district’ de la tabla ‘address’ donde el
+distrito sea Texas.
+3. Seleccionar todas las columnas de la tabla ‘payment’ cuyo campo ‘amount’
+sea mayor que 11.
+4. Visualizar todos los actores de la tabla ‘actor’ cuyo ‘first_name’ sea Penelope.
+Probar varias formas de escribir el nombre para comprobar si obtenemos
+resultados diferentes.
+5. Repite el proceso anterior utilizando BINARY.
+6. Visualizar las columnas ‘first_name’ y ‘last_name’ de la tabla ‘customer’ de
+aquellos clientes que estén inactivos (En otras palabras, que el campo ‘active’
+sea igual a 0)
+7. Probar la consulta anterior cambiando el 0 por False. ¿Funciona?
+8. Piensa qué resultado deberían dar las siguientes querys:
+select first_name,last_name,active from customer where
+active;
+select first_name,last_name,active from customer where
+!active;
+*/
 
+-- 1. Descargar la base de datos ‘sakila’ e instalarla en MySQL Workbench.
+USE sakila;
+-- 2. Seleccionar los campos ‘address’ y ‘district’ de la tabla ‘address’ donde el distrito sea Texas.
+Select address, district 
+	from sakila.address 
+		where district="Texas";
+-- 3. Seleccionar todas las columnas de la tabla ‘payment’ cuyo campo ‘amount’ sea mayor que 11.
+Select * from sakila.payment where amount > 11;
+-- 4. Visualizar todos los actores de la tabla ‘actor’ cuyo ‘first_name’ sea Penelope.
+Select * from sakila.actor where first_name = "Penelope";
+	-- Probar varias formas de escribir el nombre para comprobar si obtenemos resultados diferentes.
+	Select * from sakila.actor where first_name = "PeneLOPe";
+	Select * from sakila.actor where first_name = "penelope"; 
+-- 5. Repite el proceso anterior utilizando BINARY.
+Select * from sakila.actor where binary first_name = "Penelope";
+Select * from sakila.actor where binary first_name = "PeneLOPe";
+Select * from sakila.actor where binary first_name = "penelope";
+Select * from sakila.actor where binary first_name = "PENELOPE";
+-- 6. Visualizar las columnas ‘first_name’ y ‘last_name’ de la tabla ‘customer’ de
+	-- aquellos clientes que estén inactivos (En otras palabras, que el campo ‘active’ sea igual a 0)
+Select * from sakila.customer where active=0;
+-- 7. Probar la consulta anterior cambiando el 0 por False. ¿Funciona?
+Select * from sakila.customer where active=false;
+-- 8. Piensa qué resultado deberían dar las siguientes querys:
+select first_name,last_name,active from sakila.customer 
+	where active;
+    -- En esta primera query, el resultado deberían ser todos los clientes de la tabla customer con active = 1
+select first_name,last_name,active from sakila.customer 
+	where !active;
+
+/*
+!active --> FALSE
+ active --> TRUE
+*/
 
 
 
